c9629270bab47f09a82f1e11fb0636060dfffd4f
Samuelfaure <sfaure@student.42.fr>
2017-10-09 17:04:24 +0200

    Changed GET request base adresses depending on dev or prod environment, added SIREN endpoint

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,16 @@
-export default {
-  // baseAdress: 'http://sirene.entreprise.api.gouv.fr/full_text/', // TODO : replace by HTTPS
-  baseAdress: 'http://localhost:3000/full_text/', // TODO : replace by HTTPS
-  // baseAdressSiret: 'http://sirene.entreprise.api.gouv.fr/siret/' // TODO : replace by HTTPS
-  baseAdressSiret: 'http://localhost:3000/siret/' // TODO : replace by HTTPS
+const localAdresses = {
+  baseAdress: 'http://localhost:3000/full_text/',
+  baseAdressSiret: 'http://localhost:3000/siret/',
+  baseAdressSiren: 'http://localhost:3000/siren/'
 }
+
+const productionAdresses = {
+  baseAdress: 'https://sirene.entreprise.api.gouv.fr/full_text/',
+  baseAdressSiret: 'https://sirene.entreprise.api.gouv.fr/siret/',
+  baseAdressSiren: 'https://sirene.entreprise.api.gouv.fr/siren/'
+}
+
+const adressesToExport = process.env.NODE_ENV === 'production'
+  ? productionAdresses : localAdresses
+
+export default adressesToExport
